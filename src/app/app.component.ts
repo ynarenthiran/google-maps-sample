008d6ed4261f71e82479ab1c93c8fe5ab7e69bd0
ynarenthiran <narenthiran4019@gmail.com>
2019-12-31 19:57:22 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from 
-  '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'my-app',
